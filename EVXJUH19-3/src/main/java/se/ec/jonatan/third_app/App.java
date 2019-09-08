a969d67de31e0b15e7e5f47d1fd8c2a4a191ccdf
Jonatan <Jonatan@DESKTOP-86JMF5R>
2019-09-08 16:37:01 +0200

    Third app

---
+++ EVXJUH19-3/src/main/java/se/ec/jonatan/third_app/App.java
@@ -0,0 +1,13 @@
+package se.ec.jonatan.third_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
